01fbbafbb9b2ae504d20b2612b29ef28de8ba38d
matcls <manueltorresvesga@gmail.com>
2020-12-09 12:36:30 -0500

    Add puppet manifest to install and configure nginx server
    Requirements:
    
    Nginx should be listening on port 80
    When querying Nginx at its root / with a GET request, it must return a page that contains the string Holberton School
    The redirection must be a 301 Moved Permanently

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+# Install Nginx web server (w/ Puppet)
+# Requirements:
+# Nginx should be listening on port 80
+# 4 # Return a page that contains the string "Holberton School" on GET request
+# The redirection must be a 301 Moved Permanently
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx', # Double check
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+file_line {  'sites-default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'root /var/www/html;',
+  # Could use a shell command but this is more clean
+  line   => 'rewrite ^redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service {  'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
