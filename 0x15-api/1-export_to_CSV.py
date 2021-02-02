37c1b4e6caf5d9bd553743957b9e120cd813f8b8
matcls <manueltorresvesga@gmail.com>
2021-02-01 20:33:57 -0500

    Add python script that export Completed/TODOs  using a REST API:
        REST API: https://jsonplaceholder.typicode.com/
    
        Requirements
    	The script must accept an integer as a parameter, which is the employee ID
    	Export data in the CSV format.
    	Records all tasks that are owned by this employee
    	Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
    
    File name must be: USER_ID.csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Return information about a employee TO-DO list progress.
+
+Using https://jsonplaceholder.typicode.com/ API.
+"""
+
+
+def do_request():
+    """Request TODO list from a given employee id using the REST API.
+
+    Returns:
+        String: Completed/Total tasks
+    """
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        return print('USAGE:', __file__, '<employee id>')
+
+    try:
+        employee_id = int(argv[1])
+    except ValueError:
+        return print('Employee id must be an integer')
+
+    url = "https://jsonplaceholder.typicode.com/"
+    user_info = requests.get(url + "users/{}".format(employee_id)).json()
+    tasks = requests.get(url + "todos", params={"userId": employee_id}).json()
+
+    completed = [task.get("title") for task in tasks
+                 if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_info.get("name"), len(completed), len(tasks)))
+    [print("\t {}".format(c)) for c in completed]
+    export_to_csv(employee_id, user_info, tasks)
+    return employee_id, user_info, tasks
+
+
+def export_to_csv(employee_id, user_info, tasks):
+    """Export the empployee TODO data in a CSV format file."""
+    import csv
+
+    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [employee_id,
+             user_info.get("name"),
+             task.get("completed"),
+             task.get("title")])
+            for task in tasks]
+
+
+if __name__ == "__main__":
+    data = do_request()
+    export_to_csv(*data)
