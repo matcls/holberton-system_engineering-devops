f617b4c45f2d2baae304c1bd6619d77919ad3038
matcls <manueltorresvesga@gmail.com>
2021-02-01 20:20:57 -0500

    use more appropiate variable user_info instead user_name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,13 +23,13 @@ def do_request():
         return print('Employee id must be an integer')
 
     url = "https://jsonplaceholder.typicode.com/"
-    user_name = requests.get(url + "users/{}".format(employee_id)).json()
+    user_info = requests.get(url + "users/{}".format(employee_id)).json()
     tasks = requests.get(url + "todos", params={"userId": employee_id}).json()
 
     completed = [task.get("title") for task in tasks
                  if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user_name.get("name"), len(completed), len(tasks)))
+        user_info.get("name"), len(completed), len(tasks)))
     [print("\t {}".format(c)) for c in completed]
 
 
