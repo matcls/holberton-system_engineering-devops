835cafe15a678e42c924466810872c6424ec2f88
matcls <manueltorresvesga@gmail.com>
2021-02-04 14:54:02 -0500

    Make it explicit not to follow redirects.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,9 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for the given subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'
     headers = {'User-Agent': 'matv:1.0.0'}
-    request = get(url.format(subreddit), headers=headers)
+    request = get(url.format(subreddit),
+                  headers=headers,
+                  allow_redirects=False)
     if request.status_code != 200:
         return 0
     return request.json().get('data', {}).get('subscribers', 0)
