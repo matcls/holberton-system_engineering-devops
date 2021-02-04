b86ca4826304ae94e828bf1a82bd70785cf75b8e
matcls <manueltorresvesga@gmail.com>
2021-02-04 15:41:51 -0500

    Add script that prints the titles of the first 10 hot posts
    * For a given subreddit.
    * Prints "None" if an invalid subreddit is given.
    * Not follow redirects.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Queries the Reddit API.
+
+Prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+from requests import get
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts
+     listed for a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/.json?sort=hot&limit=10'
+    headers = {'User-Agent': 'matv:1.0.0'}
+    request = get(url.format(subreddit),
+                  headers=headers,
+                  allow_redirects=False)
+    if request.status_code != 200:
+        print("None")
+        return
+    datas = request.json().get('data', {}).get('children', {})
+    for post in datas:
+        print(post.get('data', {}).get('title'))
