ab25d77fcafe9d268bd5fb5de7c723e0a7065e20
matcls <manueltorresvesga@gmail.com>
2021-02-04 22:58:00 -0500

    Refactor count_words function
    Now work as expected and is more legible

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,51 +5,46 @@ Recursively obtains the count of words in all hot post titles.
 """
 from requests import get
 after = ""
-word_count = {}
 
 
 def count_words(subreddit, word_list):
     """Print the count of words fro, all hot post titles."""
+    hot_list = recurse(subreddit)
+    word_dict = {}
+
+    if not hot_list:
+        return None
+    else:
+        word_dict = {word: 0 for word in word_list}
+
+    for title in hot_list:
+        title_split = title.split(" ")
+        for word in title_split:
+            for s_word in word_list:
+                if word.lower() == s_word.lower():
+                    word_dict[s_word] += 1
+
+    for k, v in sorted(word_dict.items(), key=lambda kv: kv[1], reverse=True):
+        if v != 0:
+            print("{}: {}".format(k, v))
+
+
+def recurse(subreddit, hot_list=[]):
+    """Return a list with the titles of all hot articles for subreddit."""
     global after
-    global word_count
-    params = {
-        "after": after,
-        "limit": 100
-    }
-    url = 'https://www.reddit.com/r/{}/hot.json'
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'
     headers = {'User-Agent': 'matv:1.0.0'}
-    request = get(url.format(subreddit,
-                  params=params),
+    request = get(url.format(subreddit, after),
                   headers=headers,
                   allow_redirects=False)
     if request.status_code != 200:
         return None
+    if after is None:
+        return hot_list
+    data = request.json().get('data').get('children')
+    for post in data:
+        hot_list.append(post.get('data').get('title'))
 
-    data = request.json()
-
-    hot_titles_list = [child.get("data").get("title")
-                       for child in data
-                       .get("data")
-                       .get("children")]
-    if not hot_titles_list:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_titles_list:
-        split_words = title.split(" ")
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    after = data.get("data").get("after")
-    if not data:
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print("{} : {}".format(k, v)) for k, v in sorted_counts if v != 0]
-    else:
-        return count_words(subreddit, word_list)
+    after = request.json().get('data').get('after')
+    recurse(subreddit, hot_list)
+    return(hot_list)
