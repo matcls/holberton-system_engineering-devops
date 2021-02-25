c6f0b62147bbb377879c7a21b300b6ad05fc7fe6
matcls <manueltorresvesga@gmail.com>
2021-02-25 08:57:24 -0500

    Add Puppet manifest to change soft and hard file limit
    Change limit for user holberton

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Change file limit for holberton user
+
+exec { 'hard-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
